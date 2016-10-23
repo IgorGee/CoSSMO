76ef3349647fdbc9cbe8ccf5483278496fbd56f4
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-10-22 21:35:13 -0400

    Normalize styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Navigation from '../Navigation'
 import Footer from '../Footer'
 
+import 'react-toolbox/lib/commons.scss';
+
 const App = props => {
   return (
     <div>
