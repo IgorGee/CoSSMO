492a02c36a5fd0c00a84d55f69c9698b1643d221
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-11-25 03:18:56 -0500

    Treat /images as a component
    Much more flexible and readible image now imo.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,23 +2,26 @@ import React from 'react'
 
 import Field from './Field/'
 import s from './styles.scss'
+import Images from '../../images'
+
+const image = Images.traffic
 
 const computerSimulation = [
-  { name: "Virtual Reality", image: "images/traffic.png" },
-  { name: "Experimentation", image: "images/traffic.png" },
-  { name: "Simulation Methods", image: "images/traffic.png" },
-  { name: "Dimension Reduction", image: "images/traffic.png" }
+  { name: "Virtual Reality", image },
+  { name: "Experimentation", image },
+  { name: "Simulation Methods", image },
+  { name: "Dimension Reduction", image }
 ]
 const optimization = [
-  { name: "Decision Making", image: "images/traffic.png" },
-  { name: "Control of Dynamic Processes", image: "images/traffic.png" },
-  { name: "Machine Learning", image: "images/traffic.png" },
-  { name: "Evolutionary Computing", image: "images/traffic.png" }
+  { name: "Decision Making", image },
+  { name: "Control of Dynamic Processes", image },
+  { name: "Machine Learning", image },
+  { name: "Evolutionary Computing", image }
 ]
 const stochasticModelling= [
-  { name: "Markov Chains", image: "images/traffic.png" },
-  { name: "Markov Processes", image: "images/traffic.png" },
-  { name: "Financial Modelling", image: "images/traffic.png" }
+  { name: "Markov Chains", image },
+  { name: "Markov Processes", image },
+  { name: "Financial Modelling", image }
 ]
 
 const Home = props => {
---
+++ src/images/index.js
@@ -0,0 +1,3 @@
+export default {
+  traffic: require('./cunyLogo.png')
+}
