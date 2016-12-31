a6ea7fe8ee2a58f6230281ec32c48124e249a085
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-12-31 02:22:29 -0500

    Add subtopics to Project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,33 @@
 // @flow
 import React from 'react'
+import { Tab, Tabs } from 'react-toolbox'
 
-const Projects = () => (
-  <h1>Projects</h1>
-)
+import s from './styles.scss'
 
-export default Projects
+export default class Projects extends React.Component {
+  state = {
+    index: 0
+  }
+
+  handleTabChange = index => {
+    this.setState({ index })
+  }
+
+  render() {
+    return (
+      <section className={s.root}>
+        <Tabs index={this.state.index} onChange={this.handleTabChange} fixed>
+          <Tab label="Outreach">
+            <small>First Content</small>
+          </Tab>
+          <Tab label="Research">
+            <small>Second Content</small>
+          </Tab>
+          <Tab label="Education">
+            <small>Third Content</small>
+          </Tab>
+        </Tabs>
+      </section>
+    )
+  }
+}
