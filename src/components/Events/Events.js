f84e05e8cec390abc267acf893cf491bf887fffa
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-01-08 01:39:43 -0500

    Add Conference Presentations

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,8 +1,23 @@
 // @flow
 import React from 'react'
 import { Tabs, Tab } from 'react-toolbox'
+import ConferencePresentationCard from './ConferencePresentationCard'
 
 import s from './styles.scss'
+
+import conferencePresentationObjects from '../../public_data/presentations/conference_presentations.json'
+
+const conferencePresentationCards = conferencePresentationObjects
+  .map((presentation, index) => (
+    <ConferencePresentationCard
+      key={index}
+      title={presentation.title}
+      speakers={presentation.speakers}
+      event={presentation.event}
+      location={presentation.location}
+      date={`${presentation.date.month}/${presentation.date.year}`}
+    />
+))
 
 export default class Events extends React.Component {
   state = {
@@ -18,6 +33,8 @@ export default class Events extends React.Component {
       <section className={s.root}>
         <Tabs index={this.state.index} onChange={this.handleTabChange} fixed>
           <Tab label="Presentations">
+            <h3>Conference Presentations</h3>
+            {conferencePresentationCards}
           </Tab>
           <Tab label="Visitors">
           </Tab>
