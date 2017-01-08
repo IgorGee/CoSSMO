c9c0ab9dfb808100183ff29e878789d8f9015ebe
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-01-08 01:45:23 -0500

    Add Invited Lectures Cards

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -2,10 +2,12 @@
 import React from 'react'
 import { Tabs, Tab } from 'react-toolbox'
 import ConferencePresentationCard from './ConferencePresentationCard'
+import InvitedLectureCard from './InvitedLectureCard'
 
 import s from './styles.scss'
 
 import conferencePresentationObjects from '../../public_data/presentations/conference_presentations.json'
+import invitedLectureObjects from '../../public_data/presentations/invited_lectures.json'
 
 const conferencePresentationCards = conferencePresentationObjects
   .map((presentation, index) => (
@@ -16,6 +18,17 @@ const conferencePresentationCards = conferencePresentationObjects
       event={presentation.event}
       location={presentation.location}
       date={`${presentation.date.month}/${presentation.date.year}`}
+    />
+))
+
+const invitedLectureCards = invitedLectureObjects
+  .map((lecture, index) => (
+    <InvitedLectureCard
+      key={index}
+      title={lecture.title}
+      location={lecture.location}
+      host={lecture.host}
+      date={`${lecture.date.month}/${lecture.date.year}`}
     />
 ))
 
@@ -35,6 +48,8 @@ export default class Events extends React.Component {
           <Tab label="Presentations">
             <h3>Conference Presentations</h3>
             {conferencePresentationCards}
+            <h3>Invited Lectures</h3>
+            {invitedLectureCards}
           </Tab>
           <Tab label="Visitors">
           </Tab>
