7c7006b7efa157679d489c428488d81c2a7bcd63
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-01-08 00:39:57 -0500

    Create initial layout for Events page

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,8 +1,30 @@
 // @flow
 import React from 'react'
+import { Tabs, Tab } from 'react-toolbox'
 
-const Events = () => (
-  <h1>Events</h1>
-)
+import s from './styles.scss'
 
-export default Events
+export default class Events extends React.Component {
+  state = {
+    index: 0
+  }
+
+  handleTabChange = index => {
+    this.setState({ index })
+  }
+
+  render() {
+    return (
+      <section className={s.root}>
+        <Tabs index={this.state.index} onChange={this.handleTabChange} fixed>
+          <Tab label="Presentations">
+          </Tab>
+          <Tab label="Visitors">
+          </Tab>
+          <Tab label="News">
+          </Tab>
+        </Tabs>
+      </section>
+    )
+  }
+}
