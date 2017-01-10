61070758a8c1284ae10cc8473374388bf59e1989
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-01-09 20:07:49 -0500

    Rearrange content

--- src/components/Opportunities/Opportunities.js
+++ src/components/Opportunities/Opportunities.js
@@ -28,12 +28,7 @@ export default class Opportunities extends React.Component {
             <p>Massive processing of statistical data that allows computers to discover patterns and act “intelligently”. Most notably, the recent success of the IBM Watson algorithms exemplifies the power that such methodologies will acquire in our immediate future.</p>
 
             <h5>Evolutionary Optimization Algorithms</h5>
-            <p>Techniques for optimization of large complex systems have recently been proposed that mimic the manner in which genetic mutations succeed in natural selection, and others mimic the manner in which animal colonies (notably ants and bees) communicate simple individual directives in order to produce a coherent behavior that results in intelligent actions with purpose that goes beyond what each individual can grasp.</p>
-
-            <h3>Affiliated Member</h3>
-            <p>Any Faculty member at CUNY can be affiliated with the Institute. Affiliated members must contribute towards the mission of CoSSMO, usually in the form of collaboration in a project. All supervisors of active student members are also affiliated members. To apply for membership, identify one or more projects in either outreach, research or education that you wish to supervise or co-supervise. You may either join a current program or suggest a new one.</p>
-            <p>It is expected that within your first year you will submit at least one research grant or research contract through CoSSMO and that you attend and contribute to our seminars regularly.</p>
-            <p>Memberships will be granted by the EC and will be subject to revision every three years.</p><br />
+            <p>Techniques for optimization of large complex systems have recently been proposed that mimic the manner in which genetic mutations succeed in natural selection, and others mimic the manner in which animal colonies (notably ants and bees) communicate simple individual directives in order to produce a coherent behavior that results in intelligent actions with purpose that goes beyond what each individual can grasp.</p><br />
 
             <b>Resident Scholars in Emerging Technologies will be hired either:</b>
             <ul>
@@ -41,6 +36,7 @@ export default class Opportunities extends React.Component {
               <li>Visitors on Sabbatical from their institutions (national and international) whose stay will be funded by seeking special collaborative grants or who bring their own funding.</li>
               <li>Invited CUNY professors on teaching release from their college, upon approval of the appointment by the Provost of the CUNY college where the affiliation of the professor is.</li>
             </ul>
+            <br />
 
             <a
               target="_blank"
@@ -54,6 +50,11 @@ export default class Opportunities extends React.Component {
                 style={{ width: '100%' }}
               />
             </a>
+
+            <h3>Affiliated Member</h3>
+            <p>Any Faculty member at CUNY can be affiliated with the Institute. Affiliated members must contribute towards the mission of CoSSMO, usually in the form of collaboration in a project. All supervisors of active student members are also affiliated members. To apply for membership, identify one or more projects in either outreach, research or education that you wish to supervise or co-supervise. You may either join a current program or suggest a new one.</p>
+            <p>It is expected that within your first year you will submit at least one research grant or research contract through CoSSMO and that you attend and contribute to our seminars regularly.</p>
+            <p>Memberships will be granted by the EC and will be subject to revision every three years.</p><br />
           </Tab>
           <Tab label="Students">
             Under Construction
